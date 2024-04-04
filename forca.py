7a333795da49a6a820d5666f8b19e602024cb88e
tecsinthya <137504450+tecsinthya@users.noreply.github.com>
2024-04-04 09:23:40 -0300

    criando jogo da forca pq o prof pediu

---
+++ forca.py
@@ -0,0 +1,62 @@
+def jogar():
+     print("******************************")
+     print("***Bem vindo ao jogo forca!***")
+     print("******************************")
+     
+     palavra_secreta = "maça".upper()
+     letras_acertadas = ["_" for letra in palavra_secreta]
+     letras_acertadas2 = []
+     
+     for letra in (palavra_secreta):
+         letras_acertadas2.append("_")
+         
+         enforcou = False 
+         acertou = False 
+         erros = 0 
+         
+         print(letras_acertadas)
+         
+         while (not enforcou and not acertou):
+             chute = input("Qual letra?")
+             chute = chute.strip().upper()  
+              
+             if (chute in palavra_secreta):
+                 index = 0 
+                 for letra in palavra_secreta:
+                     if (chute == letra):
+                         letras_acertadas[index] = letra 
+                     index += 1 
+             else:
+                 erros += 1 
+        
+         enforcou = eros == 6 
+         acertou = "_" not in letras_acertadas
+         print(letras_acertadas)
+        
+         if (acertou):
+             print("voce ganhou!!")
+         else:
+             print("voce perdeu!!")
+         print("fim de jogo")
+        
+    
+if (__name__ == "__main__"):
+   jogar()
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
